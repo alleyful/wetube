63d68ff476c0eb6a179604da8f1720c2cb9cba11
alley <alley@snaps.com>
2019-02-21 17:12:25 +0900

    express router#1

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
+import { userRouter } from './router';
+
+const app = express();
+
+const handleHome = (req, res) => res.send('Hello from Home!!!~~');
+
+const handleProfile = (req, res) => res.send('You are on Profile');
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan('dev'));
+
+app.get('/', handleHome);
+
+app.get('/profile', handleProfile);
+
+app.use('/user', userRouter)
+
+export default app;
