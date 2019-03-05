025f64075b63ede2b8e51dee7f5d36b9b7e75047
alley <alley@snaps.com>
2019-03-05 14:27:37 +0900

    add routers

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
 import { userRouter } from './router';
+import globalRouter from './routers/globalRouter';
+import userRouter from './routers/userRouter';
+import videoRouter from './routers/videoRouter';
 
 const app = express();
-
-const handleHome = (req, res) => res.send('Hello from Home!!!~~');
-
-const handleProfile = (req, res) => res.send('You are on Profile');
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -17,10 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan('dev'));
 
-app.get('/', handleHome);
-
-app.get('/profile', handleProfile);
-
-app.use('/user', userRouter)
+app.use('/', globalRouter);
+app.use('/user', userRouter);
+app.use('video', videoRouter);
 
 export default app;
