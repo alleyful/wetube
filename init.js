e2448344ccbeac00ae93120a98355fcf61de673d
alleyful <alleyful@naver.com>
2019-09-18 13:49:53 +0900

    [day3]

--- init.js
+++ init.js
@@ -5,7 +5,7 @@ dotenv.config();
 import './models/Video';
 import './models/Comment';
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4600;
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
