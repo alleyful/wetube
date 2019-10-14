cc407de69edc0508a527ec485b2a098350ea2781
alleyful <alleyful@naver.com>
2019-10-15 00:04:56 +0900

    [#7] Relationships and Route Protection

--- controllers/userController.js
+++ controllers/userController.js
@@ -116,7 +116,43 @@ export const userDetail = async (req, res) => {
 	}
 };
 
-export const editProfile = (req, res) =>
+export const getEditProfile = (req, res) =>
 	res.render("editProfile", { pageTitle: "Edit Profile" });
-export const changePassword = (req, res) =>
+
+export const postEditProfile = async (req, res) => {
+	const {
+		body: { name, email },
+		file
+	} = req;
+	try {
+		await User.findByIdAndUpdate(req.user.id, {
+			name,
+			email,
+			avatarUrl: file ? file.path : req.user.avatarUrl
+		});
+		res.redirect(routes.me);
+	} catch (error) {
+		res.redirect(routes.editProfile);
+	}
+};
+
+export const getChangePassword = (req, res) =>
 	res.render("changePassword", { pageTitle: "Change Password" });
+
+export const postChangePassword = async (req, res) => {
+	const {
+		body: { oldPassword, newPassword, newPassword1 }
+	} = req;
+	try {
+		if (newPassword !== newPassword1) {
+			res.status(400);
+			res.redirect(`/users/${routes.changePassword}`);
+			return;
+		}
+		await req.user.changePassword(oldPassword, newPassword);
+		res.redirect(routes.me);
+	} catch (error) {
+		res.status(400);
+		res.redirect(`/users/${routes.changePassword}`);
+	}
+};
