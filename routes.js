98dc1e738a2d1734b229348479b2960eaba9b72a
alleyful <alleyful@naver.com>
2019-10-18 15:05:48 +0900

    [#10] API + AJAX #1

--- routes.js
+++ routes.js
@@ -30,6 +30,11 @@ const GITHUB_CALLBACK = "/auth/github/callback";
 const FB = "/auth/facebook";
 const FB_CALLBACK = "/auth/facebook/callback";
 
+// API
+
+const API = "/api";
+const REGISTER_VIEW = "/:id/view";
+
 const routes = {
 	home: HOME,
 	join: JOIN,
@@ -73,7 +78,9 @@ const routes = {
 	githubCallback: GITHUB_CALLBACK,
 	me: ME,
 	facebook: FB,
-	facebookCallback: FB_CALLBACK
+	facebookCallback: FB_CALLBACK,
+	api: API,
+	registerView: REGISTER_VIEW
 };
 
 export default routes;
